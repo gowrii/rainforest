a080e7a54637b04735b4798d856668b48b37b0fd
Gowri Nambiar <gowri.nambi@gmail.com>
2013-11-09 21:44:12 -0500

    Initial commit

---
+++ rainforest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainforest::Application.config.secret_key_base = 'ccf9af2c8feaa9d2259650b6af42cc779bee6669532c21f6dc44a2ab8f067dd5d78480eed0692332eb37f6cec8d694f3384ba0b9a7d111da48bc38f2f20b2abe'
